459d4fd771f6e055826555fe8e53ce11c30cb923
radioman <email@radioman.lt>
2016-08-05 20:53:05 +0300

    include headers from webrtc-checkout\src\third_party\jsoncpp\*.h

--- WebRtc.NET/update_include.bat
+++ WebRtc.NET/update_include.bat
@@ -6,3 +6,6 @@ pause
 
 xcopy /s D:\webrtc-checkout\src\third_party\libyuv\include\*.h Z:\Coding\WebRtc.NET\WebRtc.NET\include\third_party\libyuv\include\
 pause
+
+xcopy /s D:\webrtc-checkout\src\third_party\jsoncpp\*.h Z:\Coding\WebRtc.NET\WebRtc.NET\include\third_party\jsoncpp\
+pause
